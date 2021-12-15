8cf202b801e59cd45be4d3c68448bfc5b8c2ce60
Franco Agustin Cola Ibañez <agustincola54@gmail.com>
2021-12-14 23:17:20 -0300

    fix autoPresssed

--- lib/ui/shared/custom_menu_item.dart
+++ lib/ui/shared/custom_menu_item.dart
@@ -1,5 +1,5 @@
-import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
+import 'package:animate_do/animate_do.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class CustomMenuItem extends StatefulWidget {
@@ -20,29 +20,22 @@ class _CustomMenuItemState extends State<CustomMenuItem> {
   @override
   Widget build(BuildContext context) {
     return FadeInLeft(
-      delay: Duration(milliseconds: widget.delay),
+      duration: const Duration(milliseconds: 150),
       from: 50,
+      delay: Duration(milliseconds: widget.delay),
       child: MouseRegion(
-        onEnter: (_) {
-          setState(() => isHover = true);
-        },
-        onExit: (_) {
-          setState(() {
-            isHover = false;
-          });
-        },
+        onEnter: (_) => setState(() => isHover = true),
+        onExit: (_) => setState(() => isHover = false),
         child: GestureDetector(
-          onTap: widget.onPressed(),
+          onTap: () => widget.onPressed(),
           child: AnimatedContainer(
             duration: const Duration(milliseconds: 300),
             width: 150,
             height: 50,
             color: isHover ? Colors.pinkAccent : Colors.transparent,
             child: Center(
-              child: Text(
-                widget.text,
-                style: GoogleFonts.roboto(fontSize: 20, color: Colors.white),
-              ),
+              child: Text(widget.text,
+                  style: GoogleFonts.roboto(fontSize: 20, color: Colors.white)),
             ),
           ),
         ),
