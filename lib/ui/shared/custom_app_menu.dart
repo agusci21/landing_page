dbee3aa91e463de06bccfdb24c201af42f713ac8
Franco Agustin Cola Ibañez <agustincola54@gmail.com>
2021-12-14 21:13:56 -0300

    Menu Item

--- lib/ui/shared/custom_app_menu.dart
+++ lib/ui/shared/custom_app_menu.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:vertical_landing_page/ui/shared/custom_menu_item.dart';
 
 class CustomAppMenu extends StatefulWidget {
   const CustomAppMenu({Key? key}) : super(key: key);
@@ -36,28 +37,62 @@ class _CustomAppMenuState extends State<CustomAppMenu>
           });
         },
         child: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 10),
-          width: 150,
-          height: 50,
-          color: Colors.black,
-          child: Row(
-            children: [
-              AnimatedContainer(
-                duration: const Duration(milliseconds: 200),
-                curve: Curves.easeInOut,
-                width: isOpen ? 35 : 0,
-              ),
-              Text('Menú',
-                  style: GoogleFonts.roboto(color: Colors.white, fontSize: 18)),
-              const Spacer(),
-              AnimatedIcon(
-                progress: controller,
-                icon: AnimatedIcons.menu_close,
-                color: Colors.white,
-              )
-            ],
+            padding: const EdgeInsets.symmetric(horizontal: 10),
+            width: 150,
+            height: isOpen ? 308 : 50,
+            color: Colors.black,
+            child: Column(
+              children: [
+                _MenuTitle(isOpen: isOpen, controller: controller),
+                if (isOpen) ...[
+                  //Operador Spread = Si se cumple la condicion de injectan estos elementos a un arreglo
+                  CustomMenuItem(text: 'Home', onPressed: () {}),
+                  CustomMenuItem(text: 'About', onPressed: () {}),
+                  CustomMenuItem(text: 'Pricing', onPressed: () {}),
+                  CustomMenuItem(text: 'Contact', onPressed: () {}),
+                  CustomMenuItem(text: 'Location', onPressed: () {}),
+                  const SizedBox(
+                    height: 8,
+                  )
+                ]
+              ],
+            )),
+      ),
+    );
+  }
+}
+
+class _MenuTitle extends StatelessWidget {
+  const _MenuTitle({
+    Key? key,
+    required this.isOpen,
+    required this.controller,
+  }) : super(key: key);
+
+  final bool isOpen;
+  final AnimationController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 150,
+      height: 50,
+      child: Row(
+        children: [
+          AnimatedContainer(
+            duration: const Duration(milliseconds: 200),
+            curve: Curves.easeInOut,
+            width: isOpen ? 35 : 0,
           ),
-        ),
+          Text('Menú',
+              style: GoogleFonts.roboto(color: Colors.white, fontSize: 18)),
+          const Spacer(),
+          AnimatedIcon(
+            progress: controller,
+            icon: AnimatedIcons.menu_close,
+            color: Colors.white,
+          )
+        ],
       ),
     );
   }
---
+++ lib/ui/shared/custom_menu_item.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class CustomMenuItem extends StatefulWidget {
+  final String text;
+  final Function onPressed;
+
+  const CustomMenuItem({Key? key, required this.text, required this.onPressed})
+      : super(key: key);
+
+  @override
+  _CustomMenuItemState createState() => _CustomMenuItemState();
+}
+
+class _CustomMenuItemState extends State<CustomMenuItem> {
+  bool isHover = false;
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+      onEnter: (_) {
+        setState(() => isHover = true);
+      },
+      onExit: (_) {
+        setState(() {
+          isHover = false;
+        });
+      },
+      child: GestureDetector(
+        onTap: widget.onPressed(),
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 300),
+          width: 150,
+          height: 50,
+          color: isHover ? Colors.pinkAccent : Colors.black,
+          child: Center(
+            child: Text(
+              widget.text,
+              style: GoogleFonts.roboto(fontSize: 20, color: Colors.white),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
