eaceb2cd33a4486e99def8839090e8d3a1beac2b
Franco Agustin Cola Ibañez <agustincola54@gmail.com>
2021-12-14 23:30:49 -0300

    Cambiar la Uri basado en botones

--- lib/providers/page_provider.dart
+++ lib/providers/page_provider.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:universal_html/html.dart' as html;
 
 class PageProvider extends ChangeNotifier {
   PageController scrollController = PageController();
 
-  goTo(int index){
-    scrollController.animateToPage(index, duration: const Duration(milliseconds: 300), curve: Curves.easeInOut);
+  final List<String> _pages = [
+    'home',
+    'about',
+    'pricing',
+    'contact',
+    'location'
+  ];
+
+  goTo(int index) {
+    html.window.history.pushState(null, 'None', '#/${_pages[index]}');
+    scrollController.animateToPage(index,
+        duration: const Duration(milliseconds: 300), curve: Curves.easeInOut);
   }
 }
